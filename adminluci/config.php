d34d21220308bff54f8d995dfc1c27892dc53823
ChauND2010 <chaund2010@gmail.com>
2024-06-05 15:10:29 +0700

    update config
    test config

--- adminluci/config.php
+++ adminluci/config.php
@@ -3,17 +3,17 @@
 define('APPLICATION', 'Admin');
 
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8080/lucistore/adminluci/');
-define('HTTP_CATALOG', 'http://localhost:8080/lucistore/');
+define('HTTP_SERVER', 'https://enchanting-gentleness-production.up.railway.app/adminluci/');
+define('HTTP_CATALOG', 'https://enchanting-gentleness-production.up.railway.app/');
 
 // DIR
-define('DIR_OPENCART', 'C:/xampp/htdocs/lucistore/');
+define('DIR_OPENCART', './');
 define('DIR_APPLICATION', DIR_OPENCART . 'adminluci/');
 define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
 define('DIR_IMAGE', DIR_OPENCART . 'image/');
 define('DIR_SYSTEM', DIR_OPENCART . 'system/');
 define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
-define('DIR_STORAGE', 'C:/xampp/htdocs/lucistore/storage/');
+define('DIR_STORAGE', 'storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -25,10 +25,10 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
+define('DB_HOSTNAME', 'monorail.proxy.rlwy.net:24622');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'aY:-Xw-R3ZaqT79');
-define('DB_DATABASE', 'lucistore');
+define('DB_PASSWORD', 'etvCJlDrImrcYACZIqjUmFAnMsZRLSGq');
+define('DB_DATABASE', 'railway');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
